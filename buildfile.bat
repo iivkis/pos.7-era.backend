e61f43af1fcc3616f1753ce693419346652f5f43
iquzy <razmolodinivan@mail.ru>
2021-11-22 23:43:32 +0700

    remove rubbish

--- buildfile.bat
+++
@@ -1,6 +0,0 @@
-set GOOS=darwin
-go build -o "server_macos" -ldflags="-s -w" ./cmd/app/main.go
-
-set GOOS=windows
-go build -o "server_windows.exe" -ldflags="-s -w" ./cmd/app/main.go
-pause
--- internal/myservice/organizations.go
+++ internal/myservice/organizations.go
@@ -17,9 +17,9 @@ type organizations struct {
 }
 
 type organizationInput struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type organizationOutput struct {
